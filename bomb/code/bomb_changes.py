cb3d0d4fe80df30da1c4cff7415bb2d6a06ca59f
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-06-12 18:10:43 -0400

    This is a new file where we can add all the logic. It has a class that determines what happens when a phase change it is made.

--- bomb/code/bomb_changes.py
+++ bomb/code/bomb_changes.py
@@ -1,13 +1,13 @@
 # Add code to this file
 class phase_changes:
     def __init__(self, toggle_change):
-        self.toggle_change = toggle_change
+        self._toggle_change = toggle_change
 
     def get_toggle_change(self):
-        return self.toggle_change
+        return self._toggle_change
 
     def set_toggle_change(self,toggle_change):
-        self.toggle_change = toggle_change
+        self._toggle_change = toggle_change
 
-    toggle_change = property(get_toggle_change(), set_toggle_change())
+    toggle_change = property(get_toggle_change, set_toggle_change)
 
