d6c41a760ccdf75fdf15f1cac8f5d22cfca7605b
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-06-19 13:43:48 -0400

    understanding the button code

--- bomb_phases.py
+++ bomb_phases.py
@@ -316,15 +316,10 @@ class Button(PhaseThread):
                 print ("color1", self._color)
                 # was it previously pressed?
                 if (self._pressed):
-                    # check the release parameters
-                    # for R, nothing else is needed
-                    # for G or B, a specific digit must be in the timer (sec) when released
-                    if (not self._target or self._target in self._timer._sec):
-                        print("color2", self._color)
-                        self._defused = True
-                    else:
-                        self._failed = True
-                    # note that the pushbutton was released
+                    self._rgb[0].value = False
+                    self._rgb[1].value = False
+                    self._rgb[2].value = True
+                    print("color2", self._color)
                     self._pressed = False
             sleep(0.1)
 
