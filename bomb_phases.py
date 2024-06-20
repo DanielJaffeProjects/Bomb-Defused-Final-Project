23e6dacf09d786dda924571e25bfac693dcf41b6
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-06-20 10:14:53 -0400

    toggles uncommented

--- bomb_phases.py
+++ bomb_phases.py
@@ -21,9 +21,9 @@ import sys
 # the LCD display GUI
 class Lcd(Frame):
     def __init__(self, window):
-        super().__init__(window, bg="black")
+        super().__init__(window, bg="yellow")
         # make the GUI fullscreen
-        window.attributes("-fullscreen", True)
+        window.attributes("-fullscreen", False)
         # we need to know about the timer (7-segment display) to be able to pause/unpause it
         self._timer = None
         # we need to know about the pushbutton to turn off its LED when the program exits
