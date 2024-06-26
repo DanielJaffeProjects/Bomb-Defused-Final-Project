105ed9c1d7abaab2d66247f084a9a1b2d29b02d2
j-lib <jordano.liberato@gmail.com>
2024-06-26 11:09:32 -0400

    Update bomb_configs.py

--- bomb_configs.py
+++ bomb_configs.py
@@ -103,27 +103,41 @@ def generate_letter_from_number(number):
         
 def genSerial():
     wires_target = 0b11000
+    # set the digits (used in the toggle switches phase)
     serial_digits = []
     toggle_value = randint(1, 15)
-    while len(serial_digits) < 3 or toggle_value - sum(serial_digits) > 0:
+    # the sum of the digits is the toggle value
+    while (len(serial_digits) < 3 or toggle_value - sum(serial_digits) > 0):
         d = randint(0, min(9, toggle_value - sum(serial_digits)))
         serial_digits.append(d)
 
+    # set the letters (used in the jumper wires phase)
     jumper_indexes = [0] * 5
-    while sum(jumper_indexes) < 3:
+    while (sum(jumper_indexes) < 3):
         jumper_indexes[randint(0, len(jumper_indexes) - 1)] = 1
     jumper_value = int("".join([str(n) for n in jumper_indexes]), 2)
+    # the letters indicate which jumper wires must be "cut"
     jumper_letters = [chr(i + 65) for i, n in enumerate(jumper_indexes) if n == 1]
 
+    # form the serial number
     serial = [str(d) for d in serial_digits] + jumper_letters
+    # and shuffle it
     shuffle(serial)
+    # finally, add a final letter (F..Z)
     serial += [choice([chr(n) for n in range(70, 91)])]
+    # and make the serial number a string
     serial = "".join(serial)
 
+    # Generate a random number from 0 to 30 for wires phase
     wires_number = randint(0, 30)
-    wires_letter = ascii_uppercase[wires_number]
+    wires_letter = ""
+    num = wires_number
+    while num >= 0:
+        wires_letter = ascii_uppercase[num % 26] + wires_letter
+        num = num // 26 - 1
 
     return serial, toggle_value, wires_target, wires_letter, wires_number
+
 
         
 # generates the keypad combination from a keyword and rotation key
