072f632a306851f4ab5ac7c8776f061d9970dcd4
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-06-26 16:52:17 -0400

    testing audio

--- bomb_phases.py
+++ bomb_phases.py
@@ -157,13 +157,14 @@ class Lcd(Frame):
             pygame.mixer.music.play(1)
 
         # Displaying the image if you win
-        if active_phases == 0:
+        if active_phases == 2:
             winning_image = "winning image.gif"  # Replace with your image path
             img1 = PhotoImage(file=winning_image)
             self.image1 = Label(self, bg="black", image=img1)
             self.image1.image = img1  # Keep a reference to the image to prevent garbage collection
             self.image1.grid(row=2, column=1, columnspan=3, sticky=W)
             # added voice over
+            sleep(3)
             pygame.mixer.music.load("toggles defused sound.mp3")
             pygame.mixer.music.set_volume(1)
             pygame.mixer.music.play(1)
