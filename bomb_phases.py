fa68c44a529fbb865550cc9101a5f1fca681a8bb
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-06-25 21:38:02 -0400

    fixing keypad to work properly

--- bomb_phases.py
+++ bomb_phases.py
@@ -315,8 +315,8 @@ class Keypad(PhaseThread):
                 elif len(self._value) >= MAX_PASS_LEN and self._value.upper()!=self._hex_target:
                     self._failed = True
                     # self._update_callback(self._binary_code, "STRIKE")
-                # else:
-                #     self._update_callback(self._binary_code, self._value)
+                else:
+                    self._update_callback(self._binary_code, self._value)
             sleep(0.1)
         self._running = False
 
