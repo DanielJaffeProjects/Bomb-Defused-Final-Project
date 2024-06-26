e4c1f756485a4baf3b7edb4b4d9dc33ad4d75785
DanielJaffeCSCMajor <daniel.jaffe@spartans.ut.edu>
2024-06-26 09:55:29 -0400

    added audio to both bombpy and bomb phases fix little coding errors. Added beginning voice over. Turned animate to true

--- bomb_configs.py
+++ bomb_configs.py
@@ -95,7 +95,8 @@ if (RPi):
 #  the first three letters should be distinct and in the range 0..4 such that A=0, B=1, etc, to match the jumper wires
 #  the last letter should be outside of the range
 def genSerial():
-    wires_target = 0b11000
+    wires_decimal =(24)
+    wires_target = bin(wires_decimal)
     # set the digits (used in the toggle switches phase)
     serial_digits = []
     toggle_value = randint(1, 15)
